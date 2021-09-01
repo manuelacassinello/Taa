e926c3ecb4a9474983538498a9b6a7387c446fe4
Raphaelle Manet <raphaelle.manet1@gmail.com>
2021-09-01 17:20:03 +0100

    Add markers for origin and final destination

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -6,11 +6,6 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
-  const fitMapToMarkers = (map, markers) => {
-    const bounds = new mapboxgl.LngLatBounds();
-    markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
-    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-  };
 
 
   if (mapElement) { // only build a map if there's a div#map to inject into
@@ -19,6 +14,13 @@ const initMapbox = () => {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
+
+    const fitMapToMarkers = (map, markers) => {
+      const bounds = new mapboxgl.LngLatBounds();
+      markers.forEach(marker => bounds.extend([marker.long, marker.lat]));
+      map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+    };
+
 
     const markers = JSON.parse(mapElement.dataset.markers);
     console.log(typeof(markers))
