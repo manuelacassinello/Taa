4aca56962f329cd0bca6a152952d3ea892a78bc3
Manuela Rodriguez-Acosta Cassinello <mrac.in@icloud.com>
2021-09-03 14:47:29 +0100

    did scss changes, and added travelpass

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :travelpass]
 
   def home
     @home = true
+  end
+
+  def travelpass
 
   end
 end
