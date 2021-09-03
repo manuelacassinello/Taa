a313cf814937be178af0e8d5384ffbb4abed6acb
Raphaelle Manet <raphaelle.manet1@gmail.com>
2021-09-03 11:29:47 +0100

    Added a route for the create of journey

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :itineraries, only: [:new, :create, :show] do
-    resources :journeys, only: [:index, :show]
+    resources :journeys, only: [:index, :show, :create]
   end
   resources :users, only: [:show]
   resources :travelpasses, only: [:show]
