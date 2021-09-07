fd961994834a50819a7255976592b5e07039f2ad
Raphaelle Manet <raphaelle.manet1@gmail.com>
2021-09-07 14:57:35 +0100

    Tried to do the phone format thig, currently breaking

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Allows to see on phone format
+  config.hosts << "d2b1-37-157-32-162.ngrok.io"
 end
