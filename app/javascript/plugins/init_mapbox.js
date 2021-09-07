08a1100f2251e0cce7a7fc40e3b93177dce30cb4
sarah ogrady <sarahfrancesogrady@gmail.com>
2021-09-07 17:28:03 +0100

    creating journeys in ruby

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -161,23 +161,23 @@ const initMapbox = () => {
         data.duration / 60
       )} min </strong></p><ol>${tripInstructions}</ol></div>`;
 
-      const field = {journey: { distance: distance, duration: duration, transportation: transportMethod } };
-      const itinerary = document.querySelector('.itinerary-id');
-      if (itinerary) {
-        const id_itinerary = itinerary.id;
-        fetch(`/itineraries/${id_itinerary}/journeys`, {
-          method: 'POST', // or 'PUT'
-          headers: { 'Accept': "application/json", 'X-CSRF-Token': csrfToken(), 'Content-Type': 'application/json'},
-          body: JSON.stringify(field),
-        })
-        .then(response => response.json())
-        .then(field => {
-          console.log('Success:', field);
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
-      }
+      // const field = {journey: { distance: distance, duration: duration, transportation: transportMethod } };
+      // const itinerary = document.querySelector('.itinerary-id');
+      // if (itinerary) {
+      //   const id_itinerary = itinerary.id;
+      //   fetch(`/itineraries/${id_itinerary}/journeys`, {
+      //     method: 'POST', // or 'PUT'
+      //     headers: { 'Accept': "application/json", 'X-CSRF-Token': csrfToken(), 'Content-Type': 'application/json'},
+      //     body: JSON.stringify(field),
+      //   })
+      //   .then(response => response.json())
+      //   .then(field => {
+      //     console.log('Success:', field);
+      //   })
+      //   .catch((error) => {
+      //     console.error('Error:', error);
+      //   });
+      // }
     }
     map.on('load', () => {
       // make an initial directions request that
