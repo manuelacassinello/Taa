e6a839cbd533cfdb9fd3baef2831a79e2a05c0c3
Catherine Jeong <cathjeong@gmail.com>
2021-09-10 01:21:42 +0900

    chronological order of the trips history

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
   end
 
   def my_trips
-	  @my_trips = current_user.journeys.where(selected: true)
+	  my_trips = current_user.journeys.where(selected: true)
+    @my_trips = my_trips.reverse
   end
 
   def my_tree
