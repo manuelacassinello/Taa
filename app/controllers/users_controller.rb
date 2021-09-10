6842b172d365399b5afa45229fe6d6a8ceabe259
Raphaelle Manet <raphaelle.manet1@gmail.com>
2021-09-10 12:20:36 +0100

    Fix issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def my_trips
 	  my_trips = current_user.journeys.where(selected: true)
-    @my_trips = my_trips.reverse
+    @my_trips = my_trips.order(created_at: :desc)
   end
 
   def my_tree
